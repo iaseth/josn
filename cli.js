623c10e08ca8dff701620aa470d4c4a0ac717ee9
Ankur Seth <iankurseth@gmail.com>
2023-04-19 10:51:18 +0530

    Now automatically selecting if the inputPath is a directory with a single JSON file, published josn-cli@0.6.3

--- cli.js
+++ cli.js
@@ -7,36 +7,49 @@ const utils = require("./dist/utils");
 
 
 function main () {
-	const [,, filepath, ...args] = process.argv;
-
-	if (filepath === undefined) {
-		console.log("Filepath NOT provided!");
-		console.log("Usage:");
-		console.log("\tjosn data.json key1 index key2");
-		return;
-	}
-
-	if (!fs.existsSync(filepath)) {
-		console.log(`Path does NOT exist: '${filepath}'`);
-		return;
-	}
-
-	let jsonPath = filepath;
-	const stat = fs.lstatSync(filepath);
-	if (stat.isDirectory()) {
-		console.log(`Input path is a directory: '${filepath}'`);
-		const packageJsonPath = path.join(filepath, "package.json");
-		if (fs.existsSync(packageJsonPath)) {
-			console.log(`Found: ${packageJsonPath}`);
-			jsonPath = packageJsonPath;
-		} else {
-			return;
-		}
-	}
+	const [,, inputPath, ...args] = process.argv;
 
 	const keys = args.filter(a =>  utils.isObjectKey(a) || utils.isNumeric(a));
 	const singleFlags = args.filter(utils.isSingleFlag).map(a => a.slice(1));
 	const doubleFlags = args.filter(utils.isDoubleFlag).map(a => a.slice(2));
+
+	if (inputPath === undefined) {
+		console.log("Input path NOT provided!");
+		console.log("Usage:");
+		console.log("\tjosn data.json key1 index1 key2 index2");
+		return;
+	}
+
+	if (!fs.existsSync(inputPath)) {
+		console.log(`Input path does NOT exist: '${inputPath}'`);
+		return;
+	}
+
+	let jsonPath = inputPath;
+	const stat = fs.lstatSync(inputPath);
+	if (stat.isDirectory()) {
+		console.log(`Input path is a directory: '${inputPath}'`);
+		const packageJsonPath = path.join(inputPath, "package.json");
+		if (fs.existsSync(packageJsonPath)) {
+			console.log(`Found: ${packageJsonPath}`);
+			jsonPath = packageJsonPath;
+		} else {
+			const files = fs.readdirSync(inputPath);
+			const jsonFiles = files.filter(f => f.endsWith(".json"));
+			if (jsonFiles.length === 0) {
+				console.log(`Found No JSON files in directory: ${inputPath}`);
+				return;
+			} else if (jsonFiles.length === 1) {
+				console.log(`Found exactly 1 JSON file in directory: ${inputPath}`);
+				const loneJsonPath = path.join(inputPath, jsonFiles[0]);
+				console.log(`Found: ${loneJsonPath}`);
+				jsonPath = loneJsonPath;
+			} else {
+				console.log(`Found ${jsonFiles.length} JSON files in directory: ${inputPath}`);
+				return;
+			}
+		}
+	}
 
 	const jo = JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
 	let currentJo = jo;
