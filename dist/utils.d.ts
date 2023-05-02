adcc5ebe718f1fd86f677d5a8feadd3c8412604c
Ankur Seth <iankurseth@gmail.com>
2023-05-02 13:21:08 +0530

    Removed dist from repo

--- dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function isNumeric(x: any): boolean;
-export declare function isObjectKey(x: string): boolean;
-export declare function isSingleFlag(x: string): boolean;
-export declare function isDoubleFlag(x: string): boolean;
-export declare function findSimilarKey(object: any, key: string): string;
