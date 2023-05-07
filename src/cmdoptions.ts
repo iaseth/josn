8154329a5a9a8e9d4206ff53727eb855c7d83c0c
Ankur Seth <iankurseth@gmail.com>
2023-05-07 07:36:16 +0530

    Made CmdOptionsType into a class, added more options

--- src/cmdoptions.ts
+++ src/cmdoptions.ts
@@ -2,15 +2,21 @@ import { isDoubleFlag, isSingleFlag } from "./utils";
 
 
 
-export interface CmdOptionsType {
-	debug: boolean,          // debug mode on/off
-	force: boolean,          // force update even if output file exists and is newer than input file
-	list: boolean,           // just list all the input files
-	quiet: boolean,          // Quiet mode on/off
-	version: boolean,        // print JOSN version
-	watch: boolean,          // watch files for changes
-	experimental: boolean,   // experimental mode on/off
-	zen: boolean,            // zen mode on/off
+export class CmdOptions {
+	debug: boolean = false;          // debug mode on/off
+	force: boolean = false;          // force update even if output file exists and is newer than input file
+	ignoreCase: boolean = false;
+	list: boolean = false;           // just list all the input files
+	noIgnoreCase: boolean = false;
+	quiet: boolean = false;          // Quiet mode on/off
+	spaces2: boolean = false;
+	spaces4: boolean = false;
+	table: boolean = false;
+	tabs: boolean = false;
+	version: boolean = false;        // print JOSN version
+	watch: boolean = false;          // watch files for changes
+	experimental: boolean = false;   // experimental mode on/off
+	zen: boolean = false;            // zen mode on/off
 }
 
 export interface FlagType {
@@ -24,8 +30,16 @@ export interface FlagType {
 export const flags: FlagType[] = [
 	{isCommand: false, name: "debug", singleFlag: "-D", doubleFlag: "--debug", description: "Turns debug mode ON."},
 	{isCommand: false, name: "force", singleFlag: "-F", doubleFlag: "--force", description: "Force update files."},
+	{isCommand: false, name: "ignoreCase", singleFlag: "-i", doubleFlag: "--ignore-case", description: "Ignore case (default)."},
 	{isCommand: true, name: "list", singleFlag: "-L", doubleFlag: "--list", description: "List all input files."},
+	{isCommand: false, name: "noIgnoreCase", singleFlag: "-n", doubleFlag: "--no-ignore-case", description: "Do not ignore case."},
 	{isCommand: false, name: "quiet", singleFlag: "-Q", doubleFlag: "--quiet", description: "Turns quiet mode ON."},
+
+	{isCommand: false, name: "spaces2", singleFlag: "-s", doubleFlag: "--spaces2", description: "Indent with 2 spaces."},
+	{isCommand: false, name: "spaces4", singleFlag: "-S", doubleFlag: "--spaces2", description: "Indent with 2 spaces."},
+	{isCommand: false, name: "table", singleFlag: "-t", doubleFlag: "--table", description: "Print output as a table."},
+	{isCommand: false, name: "tabs", singleFlag: "-T", doubleFlag: "--tabs", description: "Indent with Tabs."},
+
 	{isCommand: true, name: "version", singleFlag: "-V", doubleFlag: "--version", description: "Print version."},
 	{isCommand: true, name: "watch", singleFlag: "-W", doubleFlag: "--watch", description: "Watch input files for changes."},
 	{isCommand: false, name: "experimental", singleFlag: "-X", doubleFlag: "--experimental", description: "Turns experimental mode ON."},
@@ -34,17 +48,8 @@ export const flags: FlagType[] = [
 
 
 
-export function getCmdOptions (flagArgs: string[]) : CmdOptionsType {
-	const cmdOptions: CmdOptionsType = {
-		debug: false,
-		force: false,
-		list: false,
-		quiet: false,
-		version: false,
-		watch: false,
-		experimental: false,
-		zen: false,
-	};
+export function getCmdOptions (flagArgs: string[]) : CmdOptions {
+	const cmdOptions: CmdOptions = new CmdOptions();
 
 	const singleFlags = flagArgs.filter(isSingleFlag);
 	const doubleFlags = flagArgs.filter(isDoubleFlag);
