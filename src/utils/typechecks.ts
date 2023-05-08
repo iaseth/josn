059c8a4c78162a48d26fc88ea8f789c4a4657ab2
Ankur Seth <iankurseth@gmail.com>
2023-05-08 12:58:44 +0530

    Added 5 typecheck funcs and along with tests
    isArray, isBoolean, isNumber, isObject and isString

--- src/utils/typechecks.ts
+++ src/utils/typechecks.ts
@@ -1,6 +1,10 @@
 
 
 
+function isArray (val: any) {
+	return Array.isArray(val);
+}
+
 function isBoolean (val: any) {
 	return typeof val === 'boolean';
 }
@@ -9,12 +13,18 @@ function isNumber (val: any) {
 	return typeof val === 'number';
 }
 
+function isObject (val: any) {
+	return typeof val === 'object' && !Array.isArray(val) && val !== null;
+}
+
 function isString (val: any) {
 	return typeof val === 'string';
 }
 
 export const typechecks = {
+	isArray,
 	isBoolean,
 	isNumber,
+	isObject,
 	isString,
 };
--- tests/utils/typechecks.test.ts
+++ tests/utils/typechecks.test.ts
@@ -2,10 +2,68 @@ const { typechecks } = require("../../dist");
 
 
 
+test("isArray", () => {
+	const { isArray } = typechecks;
+
+	expect(isArray([])).toBe(true);
+	expect(isArray([1,2,3])).toBe(true);
+
+	expect(isArray("foo")).toBe(false);
+	expect(isArray("")).toBe(false);
+	expect(isArray(20)).toBe(false);
+	expect(isArray(null)).toBe(false);
+	expect(isArray(false)).toBe(false);
+});
+
+test("isBoolean", () => {
+	const { isBoolean } = typechecks;
+
+	expect(isBoolean(true)).toBe(true);
+	expect(isBoolean(false)).toBe(true);
+
+	expect(isBoolean("foo")).toBe(false);
+	expect(isBoolean("")).toBe(false);
+	expect(isBoolean(200)).toBe(false);
+	expect(isBoolean(0)).toBe(false);
+	expect(isBoolean({})).toBe(false);
+	expect(isBoolean([])).toBe(false);
+});
+
+test("isNumber", () => {
+	const { isNumber } = typechecks;
+
+	expect(isNumber(0)).toBe(true);
+	expect(isNumber(1.2)).toBe(true);
+	expect(isNumber(-40)).toBe(true);
+	expect(isNumber(200.50)).toBe(true);
+
+	expect(isNumber("foo")).toBe(false);
+	expect(isNumber(false)).toBe(false);
+	expect(isNumber(null)).toBe(false);
+	expect(isNumber({})).toBe(false);
+});
+
+test("isObject", () => {
+	const { isObject } = typechecks;
+
+	expect(isObject({})).toBe(true);
+	expect(isObject({"foo": 20})).toBe(true);
+
+	expect(isObject("foo")).toBe(false);
+	expect(isObject("foo")).toBe(false);
+	expect(isObject("foo")).toBe(false);
+	expect(isObject("foo")).toBe(false);
+});
+
 test("isString", () => {
 	const { isString } = typechecks;
 
+	expect(isString("")).toBe(true);
 	expect(isString("foo")).toBe(true);
-});
 
-export {};
+	expect(isString(null)).toBe(false);
+	expect(isString(false)).toBe(false);
+	expect(isString(20.5)).toBe(false);
+	expect(isString({})).toBe(false);
+	expect(isString([])).toBe(false);
+});
