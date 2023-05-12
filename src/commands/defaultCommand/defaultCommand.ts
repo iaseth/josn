8cdb7d457f8c99d3ee62be2fe46f0979110dfdc2
Ankur Seth <iankurseth@gmail.com>
2023-05-12 11:19:51 +0530

    Preparing to separate object selection code from josn-cli
    Added a josnlib module with a single josn() function.
    Moved transformer and findsimilar inside josnlib.

--- src/commands/defaultCommand/defaultCommand.ts
+++ src/commands/defaultCommand/defaultCommand.ts
@@ -1,13 +1,10 @@
 import fs from 'fs';
 import path from 'path';
-import { isSlice, slice } from 'jslice';
-import { hasAColon } from 'whichtype';
 
 import { CmdOptions } from "../../cmdoptions";
 import { parseJsonFile } from '../../parse';
-import { Transformer } from './transformer';
-import { findArrayIndex, findObjectKey } from './findsimilar';
 import { demos } from '../../demos';
+import { josn } from './josnlib/josnlib';
 
 
 
@@ -80,49 +77,9 @@ export function defaultCommand (cmdOptions: CmdOptions, nonFlagArgs: string[]) {
 		return;
 	}
 
-	let currentJo = jo;
-	for (const keyArg of keyArgs) {
-		const isArray = currentJo.constructor === Array;
-		const isObject = currentJo.constructor === Object;
-		const isPrimitive = !isArray && !isObject;
-
-		if (isPrimitive) {
-			console.log(`Ignored key because value is primitive: '${keyArg}'`);
-			continue;
-		}
-
-		let newJo = null;
-
-		if (hasAColon(keyArg)) {
-			// keyArg has a colon somewhere, like ":keys" or "5:10" or "map:name"
-			if (isSlice(keyArg)) {
-				// do pythonesque array slicing
-				if (isArray) {
-					newJo = slice(currentJo, keyArg);
-				} else {
-					console.log(`Cannot slice an object: '${keyArg}'`);
-					return;
-				}
-			} else {
-				const transformer = new Transformer(keyArg, currentJo);
-				newJo = transformer.result();
-			}
-		} else {
-			if (isArray) {
-				const actualIndex = findArrayIndex(keyArg, currentJo);
-				// using at() because idx can also be negative
-				newJo = currentJo.at(actualIndex);
-			} else if (isObject) {
-				const actualKey = findObjectKey(keyArg, currentJo, cmdOptions);
-				newJo = currentJo[actualKey];
-			}
-		}
-
-		if (newJo === undefined) {
-			console.log(`Key NOT Found: '${keyArg}'`);
-			return;
-		}
-		currentJo = newJo;
+	const currentJo = josn(jo, keyArgs, cmdOptions);
+	if (!currentJo) {
+		return;
 	}
 
 	// default indentation is 1 TAB
